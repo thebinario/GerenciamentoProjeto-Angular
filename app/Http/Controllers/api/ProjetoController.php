c48ec6f9120ffc879522722c2d44af932a625303
thebinario <abimaelfrrr@gmail.com>
2021-01-31 18:04:12 -0300

    [back-end] imprementando CRUD
    Signed-off-by: thebinario <abimaelfrrr@gmail.com>

--- app/Http/Controllers/api/ProjetoController.php
+++ app/Http/Controllers/api/ProjetoController.php
@@ -19,69 +19,26 @@ class ProjetoController extends Controller
        return $projetos;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        Projetos::create($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        return Projetos::findOrFail($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $projeto = Projetos::findOrFail($id);
+        $projeto->update($request->all());
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $projeto = Projetos::findOrFail($id);
+        $projeto->delete();
     }
 }
