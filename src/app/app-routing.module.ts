fd57ae2dfb0a193f9a24bd8a41863cf72de65fab
thebinario <abimaelfrrr@gmail.com>
2021-01-31 19:29:53 -0300

    [frontend] configurando novas rotas
    Signed-off-by: thebinario <abimaelfrrr@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
+import { ListarProjetosComponent } from './pages/listar-projetos/listar-projetos.component';
+import { AtualizarPageComponent } from './pages/atualizar-page/atualizar-page.component';
+import { CadastrarPageComponent } from './pages/cadastrar-page/cadastrar-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', component: HomePageComponent}
+  {path: '', component: HomePageComponent},
+  {path: 'home', component: HomePageComponent},
+  {path: 'cadastrar', component: CadastrarPageComponent},
+  {path: 'atualizar', component: AtualizarPageComponent},
+  {path: 'projetos', component: ListarProjetosComponent},
+
 ];
 
 @NgModule({
