8bf10e13a3950b45baaf6a470bb4098f741c55f3
thebinario <abimaelfrrr@gmail.com>
2021-01-31 19:27:02 -0300

    [frontend] configurando  rotas das paginas
    Signed-off-by: thebinario <abimaelfrrr@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
