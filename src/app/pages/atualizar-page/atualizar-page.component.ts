a8e8a136dd3f3d4bdd04bdcd93a323d574cfe31d
thebinario <abimaelfrrr@gmail.com>
2021-01-31 19:26:06 -0300

    [frontend] adicionando novo componete para atualizar projetos
    Signed-off-by: thebinario <abimaelfrrr@gmail.com>

---
+++ src/app/pages/atualizar-page/atualizar-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-atualizar-page',
+  templateUrl: './atualizar-page.component.html',
+  styleUrls: ['./atualizar-page.component.css']
+})
+export class AtualizarPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
