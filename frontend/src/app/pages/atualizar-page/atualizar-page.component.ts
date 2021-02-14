fbce48fdc9c58a462353f200235d70a9e87df63a
thebinario <abimaelfrrr@gmail.com>
2021-02-14 18:06:34 -0300

    atualizando form de atualizar registros
    Signed-off-by: thebinario <abimaelfrrr@gmail.com>

--- frontend/src/app/pages/atualizar-page/atualizar-page.component.ts
+++ frontend/src/app/pages/atualizar-page/atualizar-page.component.ts
@@ -2,8 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Projeto } from 'src/app/shared/models/projeto';
 import { ProjetoService } from 'src/app/shared/services/projeto.service';
 
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {Component} from '@angular/core';
+import {MatChipInputEvent} from '@angular/material/chips';
+
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+
+export interface Fruit {
+  name: string;
+}
 
 @Component({
   selector: 'app-atualizar-page',
@@ -11,6 +19,40 @@ import { Router } from '@angular/router';
   styleUrls: ['./atualizar-page.component.css']
 })
 export class AtualizarPageComponent implements OnInit {
+
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  fruits: Fruit[] = [
+    {name: 'Lemon'},
+    {name: 'Lime'},
+    {name: 'Apple'},
+  ];
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.fruits.push({name: value.trim()});
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(fruit: Fruit): void {
+    const index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+    }
+  }
 
   constructor(private projetoService: ProjetoService, private router:Router) {}
 
